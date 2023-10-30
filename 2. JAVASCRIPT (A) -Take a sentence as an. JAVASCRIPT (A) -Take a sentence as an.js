63b7e9c852d7a21e15eee183f557ce125741cc1f
Rajalingam-sys <88856952+Rajalingam-sys@users.noreply.github.com>
2023-10-30 13:41:51 +0530

    Add files via upload

--- 2. JAVASCRIPT (A) -Take a sentence as an. JAVASCRIPT (A) -Take a sentence as an.js
+++ 2. JAVASCRIPT (A) -Take a sentence as an. JAVASCRIPT (A) -Take a sentence as an.js
@@ -1,9 +1,27 @@
 function reverseWordsInSentence(sentence) {
-  const words = sentence.split(" ");
-  const reversedWords = words.map((word) => {
-    return word.split("").reverse().join("");
-  });
-  return reversedWords.join(" ");
+  const words = [];
+  let currentWord = "";
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " " || i === sentence.length - 1) {
+      // If a space is encountered or we reach the end of the sentence, reverse the current word.
+      if (i === sentence.length - 1) {
+        currentWord += sentence[i];
+      }
+      words.push(reverseWord(currentWord));
+      currentWord = "";
+    } else {
+      currentWord += sentence[i];
+    }
+  }
+  return words.join(" ");
+}
+
+function reverseWord(word) {
+  let reversed = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversed += word[i];
+  }
+  return reversed;
 }
 
 const inputSentence = prompt("Enter a sentence:"); // Use the prompt function to get user input
