ed1264f3215153f62858326f837e8d356c06b97e
Rajalingam-sys <88856952+Rajalingam-sys@users.noreply.github.com>
2023-10-30 13:40:55 +0530

    Add files via upload

--- 1. JAVA (C) - Check if the input is pang. JAVA (C) - Check if the input is pang.java
+++ 1. JAVA (C) - Check if the input is pang. JAVA (C) - Check if the input is pang.java
@@ -1,30 +1,41 @@
 import java.util.Scanner;
 
 public class PangramChecker {
-    public static boolean isPangram(String s) {
-        boolean[] alphabet = new boolean[26];
-        for (char c : s.toCharArray()) {
-            if (Character.isLetter(c)) {
-                alphabet[Character.toLowerCase(c) - 'a'] = true;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+
+        boolean isPangram = isPangram(sentence);
+        if (isPangram) {
+            System.out.println("The input is a pangram.");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+    }
+
+    public static boolean isPangram(String sentence) {
+        // Create a boolean array to track the presence of each letter (a-z)
+        boolean[] letters = new boolean[26];
+
+        // Convert the input sentence to lowercase to make it case-insensitive
+        sentence = sentence.toLowerCase();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+            if ('a' <= ch && ch <= 'z') {
+                int index = ch - 'a';
+                letters[index] = true;
             }
         }
 
-        for (boolean letter : alphabet) {
+        // Check if all letters (a-z) are present
+        for (boolean letter : letters) {
             if (!letter) {
                 return false;
             }
         }
 
         return true;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a sentence: ");
-        String sentence = scanner.nextLine();
-        scanner.close();
-
-        boolean isPangram = isPangram(sentence);
-        System.out.println("Is the sentence a pangram? " + isPangram);
     }
 }
